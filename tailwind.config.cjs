675bcd8ba2136c10f770966467dd10ddd9374aa3
Aleksandr Zhernovoy <gernowoy@gmail.com>
2023-12-18 19:38:22 +0300

    init: start fitness app

---
+++ tailwind.config.cjs
@@ -0,0 +1,41 @@
+/* eslint-disable no-undef */
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "gray-20": "#F8F4EC",
+        "gray-50": "#EFE6E7",
+        "gray-100": "#DFCCCD",
+        "gray-500": "#5E0001",
+        "primary-100": "#FFE1E1",
+        "primary-300": "#FFA6A4",
+        "primary-500": "#FF6B67",
+        "secondary-400": "#FFCD5C",
+        "secondary-500": "#FFC133",
+      },
+      backgroundImage: () => ({
+        "gradient-yellowred":
+          "linear-gradient(90deg, #FF616B 0%, #FFC838 100%)",
+        "mobile-home": "url('./assets/HomePageGraphic.png')",
+      }),
+      fontFamily: {
+        dmsans: ["DM Sans", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+      content: {
+        evolvetext: "url('./assets/EvolveText.png')",
+        abstractwaves: "url('./assets/AbstractWaves.png')",
+        sparkles: "url('./assets/Sparkles.png')",
+        circles: "url('./assets/Circles.png')",
+      },
+    },
+    screens: {
+      xs: "480px",
+      sm: "768px",
+      md: "1060px",
+    },
+  },
+  plugins: [],
+};
