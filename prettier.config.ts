7cd107e5fa179f3209930193be71ade3f6222da2
Aleksandr Zhernovoy <gernowoy@gmail.com>
2023-12-22 15:10:19 +0300

    fix: prettier config

---
+++ prettier.config.ts
@@ -0,0 +1,7 @@
+// prettier.config.ts
+/** @type {import('prettier').Config} */
+module.exports = {
+  // plugins: [require("prettier-plugin-tailwindcss")],
+  plugins: ['prettier-plugin-tailwindcss'],
+};
+
